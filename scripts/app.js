d5a97a9775bbfd1363754b03f16683162b5c6de5
xbayazid <bayazid9006@gmail.com>
2022-09-15 22:51:59 +0600

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+const images = [
+    'images/images-1.jpg',
+    'images/images-2.jpg',
+    'images/images-3.jpg',
+    'images/images-4.jpg',
+    'images/images-5.jpg',
+    'images/images-6.jpg',
+    'images/images-7.jpg',
+    'images/images-8.jpg',
+    'images/images-9.jpg',
+];
+
+let imgIndex = 0;
+setInterval(() =>{
+
+    console.log(imgIndex);
+    imgIndex++;
+}, 1000);
