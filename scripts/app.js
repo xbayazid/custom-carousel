d44b869640f0c1ebfeef277b8e1038a2d486dd33
xbayazid <bayazid9006@gmail.com>
2022-09-16 11:15:45 +0600

    completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,24 @@
 const images = [
-    'images/images-1.jpg',
-    'images/images-2.jpg',
-    'images/images-3.jpg',
-    'images/images-4.jpg',
-    'images/images-5.jpg',
-    'images/images-6.jpg',
-    'images/images-7.jpg',
-    'images/images-8.jpg',
-    'images/images-9.jpg',
+    'images/image-1.jpg',
+    'images/image-2.jpg',
+    'images/image-3.jpg',
+    'images/image-4.jpg',
+    'images/image-5.jpg',
+    'images/image-6.jpg',
+    'images/image-7.jpg',
+    'images/image-8.jpg',
+    'images/image-9.jpg',
 ];
 
 let imgIndex = 0;
-setInterval(() =>{
 
-    console.log(imgIndex);
+const img = document.getElementById('slider-img');
+
+setInterval( () =>{
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const imgLink = images[imgIndex];
+    img.setAttribute('src', imgLink);
     imgIndex++;
-}, 1000);
+} ,1000);
